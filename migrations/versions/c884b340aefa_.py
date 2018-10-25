b596a2c3dc3b53fe95410a049b3ae0a48ccde97a
monick <monickhnm@gmail.com>
2018-10-24 21:16:52 -0300

    git

---
+++ migrations/versions/c884b340aefa_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c884b340aefa
+Revises: 510dfd768990
+Create Date: 2018-10-21 16:22:44.219042
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c884b340aefa'
+down_revision = '510dfd768990'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
